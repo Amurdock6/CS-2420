29fdac77a55449bb679775faf9b1891ab652ab32
Destroyr-of-u <todd-oldham@live.com>
2022-10-21 14:26:25 -0600

    Testing files created
    All the setup except for timing things should be done

---
+++ src/assign07/BinarySearchTreeTest.java
@@ -0,0 +1,69 @@
+package assign07;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BinarySearchTreeTest {
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testContainsAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFirst() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLast() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToArrayList() {
+		fail("Not yet implemented");
+	}
+
+}
